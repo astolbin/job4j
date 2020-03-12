56240192455fc6756557befb8f48ec6744128de3
astolbin <astolbin@ya.ru>
2020-03-12 21:16:21 +0300

    2.1. Рефакторинг - Расстояние между точками.[#213731]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,16 +1,24 @@
 package ru.job4j.condition;
 
 public class Point {
-    public static void main(String[] args) {
-        System.out.println("result (0, 0) to (2, 0) " + distance(0, 0, 2, 0));
-        System.out.println("result (3, 5) to (2, 4) " + distance(3, 5, 2, 4));
-        System.out.println("result (0, 2) to (3, 2) " + distance(0, 2, 3, 2));
-        System.out.println("result (4, 1) to (6, 3) " + distance(4, 1, 6, 3));
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double distanceX = Math.pow(x2 - x1, 2);
-        double distanceY = Math.pow(y2 - y1, 2);
+    public static void main(String[] args) {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
+    }
+
+    public double distance(Point that) {
+        double distanceX = Math.pow(this.x - that.x, 2);
+        double distanceY = Math.pow(this.y - that.y, 2);
         return Math.sqrt(distanceX + distanceY);
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -8,29 +8,45 @@ import ru.job4j.condition.Point;
 public class PointTest {
     @Test
     public void distance1() {
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+
         double expected = 2.0;
-        double out = Point.distance(0, 0, 2, 0);
+        double out = a.distance(b);
+
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void distance2() {
+        Point a = new Point(3, 5);
+        Point b = new Point(2, 4);
+
         double expected = 1.41;
-        double out = Point.distance(3, 5, 2, 4);
+        double out = a.distance(b);
+
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void distance3() {
+        Point a = new Point(0, 2);
+        Point b = new Point(3, 2);
+
         double expected = 3.0;
-        double out = Point.distance(0, 2, 3, 2);
+        double out = a.distance(b);
+
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void distance4() {
+        Point a = new Point(4, 1);
+        Point b = new Point(6, 3);
+
         double expected = 2.82;
-        double out = Point.distance(4, 1, 6, 3);
+        double out = a.distance(b);
+
         Assert.assertEquals(expected, out, 0.01);
     }
 }
