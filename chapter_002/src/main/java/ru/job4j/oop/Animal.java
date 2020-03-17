69314780c88aee530b201746a6e7cd54f38fbc0c
astolbin <astolbin@ya.ru>
2020-03-17 21:07:40 +0300

    1. Конструктор по умолчанию.[#213746]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Animal.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+public class Animal {
+    public Animal() {
+        super();
+        System.out.println("load Animal");
+    }
+
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+    }
+}
