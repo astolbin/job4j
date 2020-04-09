7fc44e706fd273153c9a38d786ee95b819e99357
astolbin <astolbin@ya.ru>
2020-04-09 22:33:30 +0300

    5. Реализовать класс Tracker[#213757]

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,50 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Tracker {
+    private final Item[] items = new Item[100];
+    private int position = 0;
+    private final Item empty = new Item("");
+
+    public Item add(Item item) {
+        item.setId(generateId());
+        items[position++] = item;
+        return item;
+    }
+
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
+
+    public Item findById(String id) {
+        for (int i = 0; i < position; i++) {
+            Item item = items[i];
+            if (item.getId().equals(id)) {
+                return item;
+            }
+        }
+        return empty;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, position);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] itemsFound = new Item[position];
+        int size = 0;
+
+        for (int i = 0; i < position; i++) {
+            Item item = items[i];
+            if (item.getName().equals(key)) {
+                itemsFound[size] = item;
+                size++;
+            }
+        }
+
+        return Arrays.copyOf(itemsFound, size);
+    }
 }
---
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -0,0 +1,58 @@
+package ru.job4j.tracker;
+
+import junit.framework.TestCase;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class TrackerTest extends TestCase {
+    private Tracker tracker;
+
+    @Override
+    public void setUp() {
+        tracker = new Tracker();
+    }
+
+    public void testWhenAddNewItemThenTrackerHasSameItem() {
+        Item item = new Item("Test 1");
+        tracker.add(item);
+
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+
+    public void testWhenTrackerNotFoundItemById() {
+        Item result = tracker.findById("Test 2");
+
+        assertNull(result.getId());
+        assertThat(result.getName(), is(""));
+    }
+
+    public void testWhenFindAllThanReturnAllItems() {
+        tracker.add(new Item("Test 1"));
+        tracker.add(new Item("Test 2"));
+        tracker.add(new Item("Test 3"));
+
+        Item[] items = tracker.findAll();
+        assertThat(items.length, is(3));
+    }
+
+    public void testWhenFindItemsByNameThan2ItemsFound() {
+        tracker.add(new Item("Test 1"));
+        tracker.add(new Item("Test 2"));
+        tracker.add(new Item("Test 2"));
+        tracker.add(new Item("Test 3"));
+
+        Item[] items = tracker.findByName("Test 2");
+        assertThat(items.length, is(2));
+    }
+
+    public void testWhenFindItemsByNameThanNoItemsFound() {
+        tracker.add(new Item("Test 1"));
+        tracker.add(new Item("Test 2"));
+        tracker.add(new Item("Test 2"));
+        tracker.add(new Item("Test 3"));
+
+        Item[] items = tracker.findByName("Test 4");
+        assertThat(items.length, is(0));
+    }
+}
