2e6e50c5608526c5a3681c761b757db0908a634c
astolbin <astolbin@ya.ru>
2020-05-02 20:52:12 +0300

    9. Написать тесты на StartUI.[#213722]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,11 +1,18 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
-import org.hamcrest.core.IsNull;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class StartUITest {
-
+    @Test
+    public void whenExit() {
+        StubInput input = new StubInput(
+            new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
+        assertThat(action.isCall(), is(true));
+    }
 }
